877abe121e76569e99cee47699a4503c2ce5b6f4
suxder <1439433131@qq.com>
2023-03-17 16:07:51 +0800

    通过webpack配置，减少代码体积

---
+++ dist/static/js/main.js
@@ -0,0 +1,2 @@
+!function(){"use strict";console.log(123*456)}();
+//# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
-import './css/index.css'
+// 想要webpack打包资源，必须显式引入该资源
+import '../css/index.css'
+import { mul } from './math'
+
+console.log(mul(123,456));
 
