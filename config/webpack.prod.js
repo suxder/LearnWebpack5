b89530ae7224204a847ec5d25768835051e2523b
suxder <1439433131@qq.com>
2023-03-17 17:40:38 +0800

    配置代码分割，优化代码运行性能

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -105,6 +105,11 @@ module.exports = {
         parallel: threads // 开启多进程
       })
     ],
+    // 代码分割配置
+    splitChunks: {
+      chunks: "all", // 对所有模块都进行分割
+      // 其他内容用默认配置即可
+    },
   },
   mode: 'production',
   devtool: "source-map"
